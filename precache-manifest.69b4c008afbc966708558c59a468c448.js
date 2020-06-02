716222f5d0ee2eded44277d4e7757704507e57fe
Alex-m18 <melnikov@tikhvin.org>
2020-06-02 23:25:45 +0300

    Updates

---
+++ precache-manifest.69b4c008afbc966708558c59a468c448.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ae5e253d65d6081aad2d8634bacfe19",
+    "url": "/ra16-composition-cards/index.html"
+  },
+  {
+    "revision": "20b268d110828bc2cb01",
+    "url": "/ra16-composition-cards/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "98fb6618f0a9d3239ed0",
+    "url": "/ra16-composition-cards/static/js/2.5888e83c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ra16-composition-cards/static/js/2.5888e83c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20b268d110828bc2cb01",
+    "url": "/ra16-composition-cards/static/js/main.d9894a79.chunk.js"
+  },
+  {
+    "revision": "72fe01015628853171a1",
+    "url": "/ra16-composition-cards/static/js/runtime-main.9b14d531.js"
+  }
+]);
